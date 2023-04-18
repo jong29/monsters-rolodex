4de754244c9b784aed4fbcc784843b35d9743efa
user <jongwoop97@gmail.com>
2023-04-18 21:04:39 +0900

    create comonents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -46,13 +46,14 @@ class App extends Component {
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' 
           onChange={onSearchChange} />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div> 
           );
-        })}
+        })} */}
+        <CardList />
       </div>
     );
   }
