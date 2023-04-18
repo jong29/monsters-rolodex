698059a056d91f36f76cc0ab7b06184e1fe1abdb
user <jongwoop97@gmail.com>
2023-04-18 20:47:23 +0900

    apply optimizations

--- src/App.js
+++ src/App.js
@@ -22,27 +22,30 @@ class App extends Component {
       },
       () => {
         console.log(this.state);
-        this.state.allMonsters = this.state.monsters;
       }
     )
     )
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+}
+
   render() {
-    
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' 
-          onChange={(event) => {
-            const searchField = event.target.value.toLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-        }} />
+          onChange={onSearchChange} />
         {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
