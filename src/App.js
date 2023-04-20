7e37c1212389191562a7072aad892dffecc09187
user <jongwoop97@gmail.com>
2023-04-21 01:20:54 +0900

    apply css to background and searchbox

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <SearchBox className='search-box' onChangeHandler = {onSearchChange} placeholder='search monsters'/>
+        <SearchBox className='monsters-search-box' onChangeHandler = {onSearchChange} placeholder='search monsters'/>
         <CardList monsters = {filteredMonsters} />
       </div>
     );
