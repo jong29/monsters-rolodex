a0522be169fb461e28062205051aeb7184c77ccc
user <jongwoop97@gmail.com>
2023-04-21 01:04:57 +0900

    componentize search box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 
 import './App.css';
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -41,15 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' 
-          onChange={onSearchChange} />
-        {/* {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div> 
-          );
-        })} */}
+        <SearchBox className='search-box' onChangeHandler = {onSearchChange} placeholder='search monsters'/>
         <CardList monsters = {filteredMonsters} />
       </div>
     );
