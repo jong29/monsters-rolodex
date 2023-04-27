74bfa91ab924c5cfe495ceb39d81bcb153350e8e
user <jongwoop97@gmail.com>
2023-04-27 21:23:49 +0900

    implement hooks usestate to searchbox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
 
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 const App = () => {
+  const [searchField, setSearchField] = useState("");
+  console.log(searchField);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  }
+
   return (
     <div className="App">
         <h1 className='app-title'>Monsters Rolodex</h1>
-        {/* <SearchBox className='monsters-search-box' onChangeHandler = {onSearchChange} placeholder='search monsters'/>
-        <CardList monsters = {filteredMonsters} /> */}
+        <SearchBox className='monsters-search-box' onChangeHandler = {onSearchChange} placeholder='search monsters'/>
+        {/* <CardList monsters = {filteredMonsters} /> */}
       </div>
   )
 }
